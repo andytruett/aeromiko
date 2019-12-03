492eb3b949c7a424f7973709eca200bf8da6512d
Andy Truett <andrew.truett@gmail.com>
2019-12-03 13:49:12 -0500

    Add methods and templates, refactor get_info.
    Pull send_command from get_info to allow for using multiple parsing
    templates against a single CLI response.
    
    Rename get_info to fsm_parse for accuracy.

--- aeromiko/aeromiko.py
+++ aeromiko/aeromiko.py
@@ -59,7 +59,22 @@ class AP:
         """
         return self.net_connect.send_command(command)
 
-    def get_info(self, command: str, template: str):
+    def send_config(self, command_list):
+        """Send list of CLI commands to access point
+
+        Parameters
+        ----------
+        command_list : list
+            list of CLI commands to send to AP
+
+        Returns
+        -------
+        str
+            CLI output resulting from AP
+        """
+        return self.net_connect.send_config_set(command_list)
+
+    def fsm_parse(self, command_response: str, template: str):
         """Get structured data from CLI command response, via textFSM template
 
         Parameters
@@ -76,8 +91,6 @@ class AP:
         """
 
         textfsm_template = pkg_resources.read_text(templates, template)
-
-        command_response = self.send_command(command)
 
         tmp = tempfile.NamedTemporaryFile(delete=False)
         with open(tmp.name, "w") as file:
@@ -114,10 +127,11 @@ class AP:
             access point hostname as defined in running configuration
         """
 
-        command = "show config running | i hostname"
+        command = "show config running | i ^hostname"
         template = "get_hostname.textfsm"
 
-        hostname_info = self.get_info(command, template)
+        command_response = self.send_command(command)
+        hostname_info = self.fsm_parse(command_response, template)
         hostname = hostname_info[0]["HOSTNAME"]
         return hostname
 
@@ -133,7 +147,8 @@ class AP:
         command = "show version"
         template = "show_version.textfsm"
 
-        version = self.get_info(command, template)
+        command_response = self.send_command(command)
+        version = self.fsm_parse(command_response, template)
         return version[0]
 
     def show_cpu(self):
@@ -150,7 +165,8 @@ class AP:
         command = "show cpu"
         template = "show_cpu.textfsm"
 
-        cpu = self.get_info(command, template)
+        command_response = self.send_command(command)
+        cpu = self.fsm_parse(command_response, template)
         return cpu[0]
 
     def show_station(self):
@@ -187,7 +203,8 @@ class AP:
         command = "show station"
         template = "show_station.textfsm"
 
-        stations = self.get_info(command, template)
+        command_response = self.send_command(command)
+        stations = self.fsm_parse(command_response, template)
         return stations
 
     def show_lldp_neighbor(self):
@@ -203,7 +220,8 @@ class AP:
         command = "show lldp neighbor"
         template = "show_lldp_neighbor.textfsm"
 
-        lldp_neighbor = self.get_info(command, template)
+        command_response = self.send_command(command)
+        lldp_neighbor = self.fsm_parse(command_response, template)
         return lldp_neighbor[0]
 
     def show_int_eth(self, interface: str):
@@ -224,7 +242,8 @@ class AP:
         command = "sh int " + interface
         template = "show_eth.textfsm"
 
-        int_eth = self.get_info(command, template)
+        command_response = self.send_command(command)
+        int_eth = self.fsm_parse(command_response, template)
         return int_eth[0]
 
     def show_int_wifi(self, interface: str):
@@ -270,7 +289,8 @@ class AP:
         command = "show int " + interface
         template = "show_wifi.textfsm"
 
-        int_wifi = self.get_info(command, template)
+        command_response = self.send_command(command)
+        int_wifi = self.fsm_parse(command_response, template)
         return int_wifi
 
     def show_acsp(self):
@@ -289,7 +309,8 @@ class AP:
         command = "sh acsp"
         template = "show_acsp.textfsm"
 
-        acsp = self.get_info(command, template)
+        command_response = self.send_command(command)
+        acsp = self.fsm_parse(command_response, template)
         return acsp
 
     def show_acsp_neighbor(self):
@@ -313,21 +334,358 @@ class AP:
         command = "show acsp neighbor"
         template = "show_acsp_neighbor.textfsm"
 
-        acsp_neighbors = self.get_info(command, template)
+        command_response = self.send_command(command)
+        acsp_neighbors = self.fsm_parse(command_response, template)
         return acsp_neighbors
 
-    def send_config(self, command_list):
-        """Send list of CLI commands to access point
+    def show_boot_param(self):
+        """Show boot parameters
 
-        Parameters
-        ----------
-        command_list : list
-            list of CLI commands to send to AP
+        Returns
+        -------
+        dict
+            DEVICE_IP
+            NETMASK
+            TFTP_SERVER_IP
+            GATEWAY_IP
+            VLAN_ID
+            NATIVE_VLAN_ID
+            NETBOOT_ALWAYS
+            NETBOOT
+            BOOT_FILE
+            NETDUMP
+            NETDUMP_FILE
+            REGION_CODE
+            COUNTRY_CODE
+        """
+        command = "show boot-param"
+        template = "show_boot_param.textfsm"
+
+        command_response = self.send_command(command)
+        boot_params = self.fsm_parse(command_response, template)
+        return boot_params
+
+    def show_acsp_channel_info(self):
+        """Show ACSP decision details for all radios
+
+        Returns
+        -------
+        dict
+            INT
+            INT_VALUE
+            STATE
+            LOWEST_COST_CHANNEL
+            LOWEST_COST
+            CHANNEL
+            CHANNEL_OVERLAP
+            CHANNEL_COST
+        """
+        command = "show acsp channel-info"
+        template = "show_acsp_channel_info.textfsm"
+
+        command_response = self.send_command(command)
+        acsp_neighbor_info = self.fsm_parse(command_response, template)
+        return acsp_neighbor_info
+
+    def show_capwap_client(self):
+        """Show capwap client information
+
+        Returns
+        -------
+        dict
+            CAPWAP_CLIENT
+            CAPWAP_TRANSPORT_MODE
+            RUN_STATE
+            CAPWAP_CLIENT_IP
+            CAPWAP_SERVER_IP
+            HIVEMANAGER_PRIMARY_NAME
+            HIVEMANAGER_BACKUP_NAME
+            CAPWAP_DEFAULT_SERVER_NAME
+            VIRTUAL_HIVEMANAGER_NAME
+            SERVER_DESTINATION_PORT
+            CAPWAP_SEND_EVENT
+            CAPWAP_DTLS_STATE
+            CAPWAP_DTLS_NEGOTIATION
+            DTLS_NEXT_CONNECT_STATUS
+            DTLS_ALWAYS_ACCEPT_BOOTSTRAP_PASSPHRASE
+            DTLS_SESSION_STATUS
+            DTLS_KEY_TYPE
+            DTLS_SESSION_CUT_INTERVAL
+            DTLS_HANDSHAKE_WAIT_INTERVAL
+            DTLS_MAX_RETRY_COUNT
+            DTLS_AUTHORIZE_FAILED
+            DTLS_RECONNECT_COUNT
+            DISCOVERY_INTERVAL
+            HEARTBEAT_INTERVAL
+            MAX_DISCOVERY_INTERVAL
+            NEIGHBOR_DEAD_INTERVAL
+            SILENT_INTERVAL
+            WAIT_JOIN_INTERVAL
+            DISCOVERY_COUNT
+            MAX_DISCOVERY_COUNT
+            RETRANSMIT_COUNT
+            MAX_RETRANSMIT_COUNT
+            PRIMARY_SERVER_TRIES
+            BACKUP_SERVER_TRIES
+            KEEPALIVES_LOST_SENT
+            EVENT_PACKET_DROP_DUE_TO_BUFFER_SHORTAGE
+            EVENT_PACKET_DROP_DUE_TO_LOSS_CONNECTION
+        """
+        command = "show capwap client"
+        template = "show_capwap_client.textfsm"
+
+        command_response = self.send_command(command)
+        capwap_client = self.fsm_parse(command_response, template)
+        return capwap_client
+
+    def show_console(self):
+        """Show console port information
+
+        Returns
+        -------
+        dict
+            CONSOLE_LINES            , # lines in console output
+            CONSOLE_TIMEOUT          , console timeout (seconds)
+            SERIAL_STATUS            , status of serial port
+            CONSOLE_OBSCURE_PASSWORDS, obscure console passwords?
+        """
+        command = "show console"
+        template = "show_console.textfsm"
+
+        command_response = self.send_command(command)
+        console = self.fsm_parse(command_response, template)
+        return console
+
+    def show_dns(self):
+        """Show DNS information
+
+        Returns
+        -------
+        dict
+            DNS_SERVER_FROM_DHCP
+            DOMAIN_NAME_SUFFIX
+            PRIMARY
+            SECONDARY
+            TERTIARY
+        """
+        command = "show dns"
+        template = "show_dns.textfsm"
+
+        command_response = self.send_command(command)
+        dns = self.fsm_parse(command_response, template)
+        return dns
+
+    def show_hivemanager(self):
+        """Show HiveManager information
+
+        Returns
+        -------
+        dict
+            HM_PRIMARY
+            HM_BACKUP
+            HM_CONNECTION
+        """
+        command = "show hivemanager"
+        template = "show_hivemanager.textfsm"
+
+        command_response = self.send_command(command)
+        hivemanager = self.fsm_parse(command_response, template)
+        return hivemanager
+
+    def show_hw_info(self):
+        """Show hardware information
+
+        Returns
+        -------
+        dict
+            ETHERNET_MAC_ADDRESS
+            SERIAL_NUMBER
+            HARDWARE_REVISION
+            PRODUCT_NAME
+            HARDWARE_ID
+            MANUFACTURING_DATE
+            NUMBER_OF_MAC_ADDRESSES
+            MANUFACTURING_VERSION
+            ANTENNA_ID
+            AEROHIVE_HARDWARE_KEY
+            HW_AUTH_DEVICE_STATUS
+            TPM_STATUS
+        """
+        command = "show hw info"
+        template = "show_hw_info.textfsm"
+
+        command_response = self.send_command(command)
+        hw_info = self.fsm_parse(command_response, template)
+        return hw_info
+
+    def show_idm(self):
+        """Show ID Manager information
+
+        Returns
+        -------
+        dict
+            IDM_CLIENT
+            IDM_PROXY_IP
+            IDM_PROXY
+            RADSEC_CERTIFICATE_STATE
+            RADSEC_CERTIFICATE_ISSUED
+            RADSEC_CERTIFICATE_EXPIRE
+        """
+        command = "show idm"
+        template = "show_idm.textfsm"
+
+        command_response = self.send_command(command)
+        idm = self.fsm_parse(command_response, template)
+        return idm
+
+    def show_ip_route(self):
+        """Show IP route information
+
+        Returns
+        -------
+        dict
+            DESTINATION
+            GATEWAY
+            NETMASK
+            FLAGS
+            METRIC
+            REF
+            USE
+            IFACE
+        """
+        command = "show ip route"
+        template = "show_ip_route.textfsm"
+
+        command_response = self.send_command(command)
+        ip_route = self.fsm_parse(command_response, template)
+        return ip_route
+
+    def show_memory(self):
+        """Show RAM information
+
+        Returns
+        -------
+        dict
+            TOTAL_MEMORY
+            FREE_MEMORY
+            USED_MEMORY
+        """
+        command = "show memory"
+        template = "show_memory.textfsm"
+
+        command_response = self.send_command(command)
+        memory = self.fsm_parse(command_response, template)
+        return memory
+
+    def show_route(self):
+        """Show route information
+
+        Returns
+        -------
+        dict
+            STA
+            NHOP
+            OIF
+            METRIC
+            UPID
+            FLAG
+        """
+        command = "show route"
+        template = "show_route.textfsm"
+
+        command_response = self.send_command(command)
+        route = self.fsm_parse(command_response, template)
+        return route
+
+    def show_snmp(self):
+        """Show SNMP information
+
+        Returns
+        -------
+        dict
+            SYSLOCATION
+            SYSCONTACT
+            COMMUNITY_NUMBERS
+        """
+        command = "show snmp"
+        template = "show_snmp.textfsm"
+
+        command_response = self.send_command(command)
+        snmp = self.fsm_parse(command_response, template)
+        return snmp
+
+    def show_ssid(self):
+        """Show SSID information
+
+        Returns
+        -------
+        dict
+            NO
+            NAME
+            FRAG
+            RTS
+            DTIM_PERIOD
+            MAX_CLIENT
+            MAC_FILTER
+        """
+        command = "show ssid"
+        template = "show_ssid.textfsm"
+
+        command_response = self.send_command(command)
+        ssid = self.fsm_parse(command_response, template)
+        return ssid
+
+    def show_clock(self):
+        """Show AP internal clock
+
+        Returns
+        -------
+        dict
+            DATE,   date
+            TIME,   time of day
+            DAY ,   day of week
+        """
+        command = "show clock"
+        template = "show_clock.textfsm"
+
+        command_response = self.send_command(command)
+        clock = self.fsm_parse(command_response, template)
+        return clock
+
+    def show_ntp(self):
+        """Show NTP information
+
+        Returns
+        -------
+        dict
+            STATE
+            INTERVAL
+            FIRST
+            SECOND
+            THIRD
+            FOURTH
+            DST
+            DST_START
+            DST_END
+        """
+        command = "show ntp"
+        template = "show_ntp.textfsm"
+
+        command_response = self.send_command(command)
+        ntp = self.fsm_parse(command_response, template)
+        return ntp
+
+    def show_timezone(self):
+        """Show timezone
 
         Returns
         -------
         str
-            CLI output resulting from AP
+            TIMEZONE
         """
-        return self.net_connect.send_config_set(command_list)
+        command = "show timezone"
+        template = "show_timezone.textfsm"
 
+        command_response = self.send_command(command)
+        timezone = self.fsm_parse(command_response, template)
+        return timezone
